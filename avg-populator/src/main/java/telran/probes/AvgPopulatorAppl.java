7f95f7f628b57c2278cbf8003736ddfb74fa35ae
AlGuron <blizgur8@gmail.com>
2024-05-07 17:52:44 +0300

    add: aws-sns-notifier, upd: AvgPopulator put to DynamoDB

--- avg-populator/src/main/java/telran/probes/AvgPopulatorAppl.java
+++ avg-populator/src/main/java/telran/probes/AvgPopulatorAppl.java
@@ -1,5 +1,6 @@
 package telran.probes;
 
+import java.util.*;
 import java.util.function.Consumer;
 
 import org.springframework.boot.SpringApplication;
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import org.springframework.data.mongodb.core.MongoTemplate;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Item;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import com.amazonaws.services.dynamodbv2.document.spec.PutItemSpec;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -31,6 +40,21 @@ public class AvgPopulatorAppl {
 	private void probeDataPopulation(ProbeData probeData) {
 		log.debug("--- Debug AvgPopulatorAppl -> Received probeData: {}", probeData);
 		ProbeDataDoc savedDoc = mongoTemplate.insert(new ProbeDataDoc(probeData));
-		log.debug("--- Debug AvgPopulatorAppl -> Document {} has been saved to Database", savedDoc);
+		log.debug("--- Debug AvgPopulatorAppl -> Document {} has been saved to MongoDB", savedDoc);
+
+		AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
+		DynamoDB dynamo = new DynamoDB(client);
+		Table table = dynamo.getTable("avg_values");
+		Map<String, Object> map = getMap(probeData);
+		table.putItem(new PutItemSpec().withItem(Item.fromMap(map)));
+		log.debug("--- Debug AvgPopulatorAppl -> Document {} has been saved to DynamoDB", map);
+	}
+
+	private Map<String, Object> getMap(ProbeData probeData) {
+		Map<String, Object> res = new HashMap<>();
+		res.put("sensorID", probeData.id());
+		res.put("timestamp", probeData.timestamp());
+		res.put("value", probeData.value());
+		return res;
 	}
 }
---
+++ aws-sns-notifier/src/main/java/telran/probes/AwsSnsNotifier.java
@@ -0,0 +1,58 @@
+package telran.probes;
+
+import java.util.function.Consumer;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClient;
+
+import lombok.extern.slf4j.Slf4j;
+import telran.probes.dto.DeviationData;
+
+@SpringBootApplication
+@Slf4j
+public class AwsSnsNotifier {
+
+	@Value("${ARN}")
+	private String arn;
+
+	@Value("${app.emails.notifier.subject}")
+	private String subjectName;
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSnsNotifier.class, args);
+
+	}
+
+	@Bean
+	Consumer<DeviationData> emailNotifierConsumer() {
+		return this::sendingMail;
+	}
+
+	void sendingMail(DeviationData deviation) {
+		log.debug("---> Received deviation data: {}", deviation);
+		AmazonSNS client = AmazonSNSClient.builder().withRegion(Regions.US_EAST_1).build();
+		long sensorId = deviation.id();
+		String topicArn = getTopicArn(sensorId);
+		log.debug("---> topic is: {}", topicArn);
+
+		String subject = getSubject(sensorId);
+		log.debug("---> subject is: {}", subject);
+
+		client.publish(topicArn, deviation.toString(), subject);
+		log.debug("---> notification has been sent");
+	}
+
+	private String getSubject(long sensorId) {
+		return subjectName + sensorId;
+	}
+
+	private String getTopicArn(long sensorId) {
+		return arn + ":sensor-" + sensorId;
+	}
+}
